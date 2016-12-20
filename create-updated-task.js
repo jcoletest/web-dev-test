a18d00d4347b83f9553725f3dbfc981de76f00c5
J Cole Morrison <j.cole.morrison@gmail.com>
2016-12-19 21:43:27 -0800

    initial commit

---
+++ create-updated-task.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const fs = require('fs')
+
+const task = require('./task-definition.json')
+const updatedTask = task
+const repo = 'ecs-webserver'
+
+const url = `${process.env.AWS_ACCOUNT_ID}.dkr.ecr.${process.env.AWS_DEFAULT_REGION}.amazonaws.com`
+
+const image = `${url}/${repo}:${process.env.CIRCLE_SHA1}`
+
+updatedTask.containerDefinitions[0].image = image
+
+updatedTask.containerDefinitions[0].environment = [
+  {
+    'name': 'NODE_ENV',
+    'value': 'staging',
+  },
+]
+
+updatedTask.containerDefinitions[0].logConfiguration.options = {
+  'awslogs-group': process.env.STAGING_API_AWSLOGS_GROUP,
+  'awslogs-region': process.env.STAGING_API_AWSLOGS_REGION,
+  'awslogs-stream-prefix': process.env.STAGING_API_AWSLOGS_PREFIX,
+}
+
+const jsonTask = JSON.stringify(updatedTask)
+
+fs.writeFile('updated-task.json', jsonTask, 'utf8')
---
+++ deploy_staging.sh
@@ -0,0 +1,49 @@
+set -e
+CLUSTER="webdevcluster"
+BUILD="$CIRCLE_SHA1"
+REGISTERYREPO="ecs-webserver"
+SERVICE="webserver"
+TASK_DEFINITION="webserver"
+
+IMAGE="$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$REGISTERYREPO:$CIRCLE_SHA1"
+
+function create_build () {
+  echo "creating prod build"
+  yarn build
+}
+
+function push_to_registry () {
+
+  echo "pushing to registry"
+
+  # Build down new version of image
+  docker build --rm=false -t $IMAGE .
+
+  eval $(aws ecr get-login --region $AWS_DEFAULT_REGION)
+
+  docker push $IMAGE
+}
+
+function update_web_service () {
+
+  echo "updating web service"
+
+  # create updated task
+  node ./create-updated-task.js
+
+  aws ecs register-task-definition \
+    --cli-input-json file://updated-task.json
+
+  aws ecs update-service --cluster $CLUSTER --service $SERVICE \
+    --task-definition $TASK_DEFINITION
+
+  # remove temp file
+  rm -rf ./updated-task.json
+}
+
+function update_ecs () {
+  push_to_registry
+  update_web_service
+}
+
+update_ecs
